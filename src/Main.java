f61a12932d701099dd81026032fde7bd0e6ee887
YigitTuncer07 <yigit_tuncer03@hotmail.com>
2023-06-07 15:12:48 +0300

    Removed unused code

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,6 @@ class Main {
 
     private static ArrayList<City> cities = new ArrayList<City>();
     private static ArrayList<City> result = new ArrayList<City>();// This is the found optimal path
-    private static ArrayList<City> temp = new ArrayList<City>();
     private static PrintWriter writer;
     private static long totalLenght = 0;// Lenght of road
     private static int numOfCities = 0;// Number of cities after half
@@ -29,10 +28,8 @@ class Main {
 
         nearestNeighborSolution(startingCity);
 
-        //regionSolution(means);
-
         System.out.println("Length of road: " + totalLenght);
-    
+
         optimizeResult();
 
         System.out.println("After Optimization Length of road: " + totalLenght);
@@ -69,9 +66,8 @@ class Main {
             current.setMeanLength(difX * difX + difY * difY);
         }
 
-
         cities.sort(Comparator.comparing(City::getMeanLength));
-        for(int i = cities.size()-1;i >= numOfCities;i--) {
+        for (int i = cities.size() - 1; i >= numOfCities; i--) {
             cities.remove(i);
         }
         return cities.get(0);
@@ -115,153 +111,6 @@ class Main {
 
     }
 
-    private static void regionSolution(double[] means){
-        double xMean = means[0];
-        double yMean = means[1];
-
-        double r = Math.sqrt(cities.get(cities.size()-1).getMeanLength());
-
-        double t =  r / numOfCities;
-
-        t *= 485;
-
-        System.out.print(t);
-        if(t <= 0){
-            t = Double.MIN_VALUE;
-        }
-
-
-        // Put all cities to proper region
-        City tp;
-        double dif;
-        long size_Cities = cities.size();
-        int loop = (int) (r / t);
-        loop += 2;
-        int j;
-        boolean ascend = true;
-
-
-        // 1. Region traverse
-        for(int i = 0; i < loop; i++) {
-            j = 0;
-            while (j < size_Cities) {
-                tp = cities.get(j);
-                if (tp.getY() - yMean >= 0) {
-                    dif = tp.getX() - xMean;
-                    if (dif >= t*i && dif <= t*(i+1)) {
-                        temp.add(tp);
-                        size_Cities--;
-                        cities.remove(j);
-                        continue;
-                    }
-                }
-                j++;
-            }
-            if (temp.size() > 0) {
-                if (ascend) {
-                    temp.sort(Comparator.comparing(City::getY));
-                } else {
-                    temp.sort(Comparator.comparing(City::getY).reversed());
-                }
-                ascend = !ascend;
-                tempToResult();
-            }
-        }
-        ascend = false;
-
-
-        // 4. Region
-        for(int i = loop; i > 0; i--) {
-            j = 0;
-            while (j < size_Cities) {
-                tp = cities.get(j);
-                if (tp.getY() - yMean < 0) {
-                    dif = tp.getX() - xMean;
-                    if (dif >= (i - 1) * t && dif <= t*i ) {
-                        temp.add(tp);
-                        size_Cities--;
-                        cities.remove(j);
-                        continue;
-                    }
-                }
-                j++;
-            }
-            if (temp.size() > 0) {
-                if (ascend) {
-                    temp.sort(Comparator.comparing(City::getY));
-                } else {
-                    temp.sort(Comparator.comparing(City::getY).reversed());
-                }
-                ascend = !ascend;
-                tempToResult();
-            }
-        }
-
-        // 3. Region
-        for(int i = 0; i < loop; i++) {
-            j = 0;
-            while (j < size_Cities) {
-                tp = cities.get(j);
-                if (tp.getY() - yMean < 0) {
-                    dif = xMean - tp.getX();
-                    if (dif >= i*t && dif <= t*(i+1)) {
-                        temp.add(tp);
-                        size_Cities--;
-                        cities.remove(j);
-                        continue;
-                    }
-                }
-                j++;
-            }
-            if (temp.size() > 0) {
-                if (ascend) {
-                    temp.sort(Comparator.comparing(City::getY));
-                } else {
-                    temp.sort(Comparator.comparing(City::getY).reversed());
-                }
-                ascend = !ascend;
-                tempToResult();
-            }
-        }
-
-        ascend = true;
-        // 2. Region
-        for(int i = loop; i > 0; i--) {
-            j = 0;
-            while (j < size_Cities) {
-                tp = cities.get(j);
-                if (tp.getY() - yMean >= 0) {
-                    dif = xMean - tp.getX();
-                    if (dif >= (i - 1) * t && dif <= t*i ){
-                        temp.add(tp);
-                        size_Cities--;
-                        cities.remove(j);
-                        continue;
-                    }
-                }
-                j++;
-            }
-            if (temp.size() > 0) {
-                if (ascend) {
-                    temp.sort(Comparator.comparing(City::getY));
-                } else {
-                    temp.sort(Comparator.comparing(City::getY).reversed());
-                }
-                ascend = !ascend;
-                tempToResult();
-            }
-        }
-
-
-        for(int i = 0; i < result.size() - 1; i++){
-            totalLenght += getDistance(result.get(i), result.get(i+1));
-        }
-        totalLenght += getDistance(result.get(0), result.get(result.size()-1));
-
-
-
-    }
-
     private static void do2Opt(ArrayList<City> result, int i, int j) {
         ArrayList<City> reversed = new ArrayList<>(result.subList(i + 1, j + 1));
         for (int k = i + 1; k <= j; k++) {
@@ -271,12 +120,10 @@ class Main {
 
     private static void optimizeResult() {
 
-
         int n = result.size();
 
         boolean foundImprovement = true;
 
-        // result.get(i).dist2(result.get(j))
         while (foundImprovement) {
             foundImprovement = false;
             for (int i = 0; i <= n - 2; i++) {
@@ -284,7 +131,8 @@ class Main {
                     if (!((i == n - 1) || (j == n - 1))) {
                         long lengthDelta = -getDistance(result.get(i), result.get(i + 1))
                                 - getDistance(result.get(j), result.get(j + 1))
-                                + getDistance(result.get(i), result.get(j)) + getDistance(result.get(i + 1), result.get(j + 1));
+                                + getDistance(result.get(i), result.get(j))
+                                + getDistance(result.get(i + 1), result.get(j + 1));
 
                         // If the length of the path is reduced, do a 2-opt swap
                         if (lengthDelta < 0) {
@@ -358,12 +206,5 @@ class Main {
             writer.printf(city.getId() + "\n");
         }
         writer.close();
-    }
-
-    private static void tempToResult(){
-        for (int i = 0; i < temp.size(); i++){
-            result.add(temp.get(i));
-        }
-        temp.clear();
     }
 }
